98470f9568259b6f3e52f101379e4d817e951047
nicorm2000 <nicorm2000@gmail.com>
2023-10-05 21:31:28 -0300

    Added testplugin script

--- Assets/TestPlugin.cs
+++ Assets/TestPlugin.cs
@@ -3,10 +3,31 @@ using UnityEngine;
 
 public class TestPlugin : MonoBehaviour
 {
+    const string packageName = "com.ramosmarin.mylibrary";
+    const string className = packageName + ".RamosMarinLogger";
+
+#if UNITY_ANDROID
+    AndroidJavaClass _pluginClass;
+    AndroidJavaObject _pluginInstance;
+
     public TextMeshProUGUI label;
+
+    private void Start()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            _pluginClass = new AndroidJavaClass(className);
+            _pluginInstance = _pluginClass.CallStatic<AndroidJavaObject>("getInstance");
+        }
+    }
+
     public void RunPlugin()
     {
         Debug.Log("Run Plugin");
-        label.text = "Test";
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            label.text = _pluginInstance.Call<string>("getLogTag");
+        }
     }
+#endif
 }
