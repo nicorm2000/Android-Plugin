8056e421c8a259b2df932d5a41daf463a64904c0
nicorm2000 <nicorm2000@gmail.com>
2023-10-22 15:43:17 -0300

    Created PopUp class

---
+++ mylibrary/src/main/java/com/ramosmarin/mylibrary/PopUp.java
@@ -0,0 +1,11 @@
+package com.ramosmarin.mylibrary;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.util.Log;
+
+public class PopUp
+{
+
+}
