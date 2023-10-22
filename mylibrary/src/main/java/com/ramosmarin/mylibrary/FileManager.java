46f0b319f4dfdaccd9822ec79d977e5c493c22c5
nicorm2000 <nicorm2000@gmail.com>
2023-10-22 16:49:15 -0300

    Created file manager class

---
+++ mylibrary/src/main/java/com/ramosmarin/mylibrary/FileManager.java
@@ -0,0 +1,15 @@
+package com.ramosmarin.mylibrary;
+
+import android.app.Activity;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileManager
+{
+    
+}
